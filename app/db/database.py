ebf6d6d92c7c24cc516d5f0ea20b19b3549be0c5
Aman2003akm <amanakm40@gmail.com>
2026-02-22 14:11:39 +0530

    Setup MySQL database connection using SQLAlchemy

---
+++ app/db/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mysql+pymysql://root:qwerty1234@localhost:3306/railway_db"
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    echo=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
