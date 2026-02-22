ad8263cdf27f184da0d39300b915177464e76bf9
Aman2003akm <amanakm40@gmail.com>
2026-02-22 14:22:33 +0530

    Setup MySQL connection, SQLAlchemy models and initialize database tables

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
 DATABASE_URL = "mysql+pymysql://root:qwerty1234@localhost:3306/railway_db"
 
 engine = create_engine(
---
+++ app/db/tables.py
@@ -0,0 +1,3 @@
+from app.db.database import Base
+from app.models.train import Train, TrainClass
+from app.models.booking import Booking
